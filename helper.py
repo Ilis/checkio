0aa1c90b7c3661b27b5a2b20f605bec0ae431f43
Ilis <ilis@inbox.ru>
2020-05-15 15:53:50 +0500

    Test mono

--- helper.py
+++ helper.py
@@ -1,22 +1,6 @@
-peoples = {
-    "Jackie": 176,
-    "Wilson": 185,
-    "Saersha": 165,
-    "Roman": 185,
-    "Abram": 169,
-    }
-
-for _name, height in peoples.items():
-    print(height)
-
-
-def tallest_people(**men):
-    print(men)
-    max_height = max(height for _name, height in men.items())
-    names = sorted([name for name, height in men.items()
-                    if height == max_height])
-    for name in names:
-        print(f"{name} : {max_height}")
-
-
-tallest_people(**peoples)
+a = 5
+a += 1
+a *= 1
+a /= 1
+a //= 2
+print(a)
