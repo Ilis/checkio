ddf7a4ef128f2892901d333cf720ee85e060e7c3
Ilis <ilis@inbox.ru>
2020-05-21 15:34:37 +0500

    reverse every asc

---
+++ O-Reilly/reverse-every-ascending.py
@@ -0,0 +1,31 @@
+def reverse_ascending(items):
+    result = []
+    sub = []
+
+    for item in items:
+        if not sub or item > sub[-1]:
+            sub.append(item)
+        else:
+            result.extend(sub[::-1])
+            sub = [item]
+
+    if sub:
+        result.extend(sub[::-1])
+        sub = []
+
+    return result
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(reverse_ascending([1, 2, 3, 4, 5]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
+    assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [10, 7, 5, 4, 8, 7, 2, 3, 1]
+    assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
+    assert list(reverse_ascending([])) == []
+    assert list(reverse_ascending([1])) == [1]
+    assert list(reverse_ascending([1, 1])) == [1, 1]
+    assert list(reverse_ascending([1, 1, 2])) == [1, 2, 1]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
