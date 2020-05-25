4798525d8c37c9016bfd1392060fe7fc543edb52
Ilis <ilis@inbox.ru>
2020-05-25 15:58:33 +0500

    family-dinner named

--- Hubspot/family-dinner-test.py
+++ Hubspot/family-dinner-test.py
@@ -1,6 +1,32 @@
-import json
 from pprint import pprint
+from collections import namedtuple
 
+
+D = {
+    'answer': 2878,
+    'input': [[[84, 43, 97, 92],
+               [25, 3, 34, 21],
+               [92, 49, 7, 27],
+               [69, 23, 10, 36],
+               [1, 36, 62, 51],
+               [53, 24, 73, 65],
+               [2, 46, 91, 45],
+               [68, 51, 48, 30],
+               [81, 79, 74, 88],
+               [32, 64, 20, 90],
+               [8, 51, 32, 21],
+               [57, 61, 14, 91],
+               [60, 55, 74, 10],
+               [2, 79, 59, 64],
+               [45, 11, 78, 52],
+               [49, 5, 72, 32],
+               [17, 93, 65, 39],
+               [67, 78, 19, 35],
+               [77, 68, 28, 31],
+               [57, 81, 65, 17],
+               [99, 58, 19, 25],
+               [34, 15, 40, 90]],
+              45]}
 
 stack_count = 5
 stack_size = 5
@@ -24,5 +50,17 @@ def fill_variants(head, rest):
         fill_variants(head + [i], rest - i)
 
 
-fill_variants([], count)
-pprint(variants)
+# fill_variants([], count)
+# pprint(variants)
+
+stacks = D["input"][0]
+pprint(stacks)
+
+St = namedtuple("St", ["i", "n", "s"])
+
+substacks = []
+stack_len = len(stacks[0])
+for i, stack in enumerate(stacks):
+    for n in range(stack_len + 1):
+        substacks.append(St(i, n, sum(stack[:n])))
+pprint(substacks)
---
+++ Pycon/ryerson-letter-grade.py
@@ -0,0 +1,34 @@
+# feel free to change table structure in any way
+
+TABLE = {
+    "A+": (90, 150),
+    "A":  (85, 89),
+    "A-": (80, 84),
+    "B+": (77, 79),
+    "B":  (73, 76),
+    "B-": (70, 72),
+    "C+": (67, 69),
+    "C":  (63, 66),
+    "C-": (60, 62),
+    "D+": (57, 59),
+    "D":  (53, 56),
+    "D-": (50, 52),
+    "F":  (0, 49)
+}
+
+
+def ryerson_letter_grade(pct: int) -> str:
+    for grade, (low, high) in TABLE.items():
+        if low <= pct <= high:
+            return grade
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(ryerson_letter_grade(45))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert ryerson_letter_grade(45) == "F"
+    assert ryerson_letter_grade(62) == "C-"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
