5321e9158c715a0c179faa8c685a8b2ba797ee8b
Ilis <ilis@inbox.ru>
2020-05-26 12:04:27 +0500

    family-dinner all tests

--- Hubspot/family-dinner-test.py
+++ Hubspot/family-dinner-test.py
@@ -1,66 +1,29 @@
 from pprint import pprint
-from collections import namedtuple
+from collections import deque
+import sys
 
 
-D = {
-    'answer': 2878,
-    'input': [[[84, 43, 97, 92],
-               [25, 3, 34, 21],
-               [92, 49, 7, 27],
-               [69, 23, 10, 36],
-               [1, 36, 62, 51],
-               [53, 24, 73, 65],
-               [2, 46, 91, 45],
-               [68, 51, 48, 30],
-               [81, 79, 74, 88],
-               [32, 64, 20, 90],
-               [8, 51, 32, 21],
-               [57, 61, 14, 91],
-               [60, 55, 74, 10],
-               [2, 79, 59, 64],
-               [45, 11, 78, 52],
-               [49, 5, 72, 32],
-               [17, 93, 65, 39],
-               [67, 78, 19, 35],
-               [77, 68, 28, 31],
-               [57, 81, 65, 17],
-               [99, 58, 19, 25],
-               [34, 15, 40, 90]],
-              45]}
-
-stack_count = 5
-stack_size = 5
-count = 20
-
-variants = []
+tests_dir = r"C:\Python\Proj\data"
+sys.path.append(tests_dir)
 
 
-def fill_variants(head, rest):
-
-    def add_variant(lst):
-        variants.append(tuple(lst))
-    if rest == 0:
-        add_variant(head + ([0] * (stack_count - len(head))))
-        return
-    if len(head) == stack_count - 1:
-        if rest <= stack_size:
-            add_variant(head + [rest])
-        return
-    for i in range(min(rest, stack_size) + 1):
-        fill_variants(head + [i], rest - i)
+from family_dinner_tests import TESTS
 
 
-# fill_variants([], count)
-# pprint(variants)
+test_stacks = TESTS["Medium"][0]
 
-stacks = D["input"][0]
+stacks = test_stacks["input"][0]
 pprint(stacks)
-
-St = namedtuple("St", ["i", "n", "s"])
+stack_len = len(stacks[0])
 
 substacks = []
-stack_len = len(stacks[0])
 for i, stack in enumerate(stacks):
     for n in range(1, stack_len + 1):
-        substacks.append(St(i, n, sum(stack[:n])))
-pprint(substacks)
+        substack = (i, n, sum(stack[:n]))
+        substacks.append(substack)
+
+queue = deque()
+for x in substacks:
+    queue.append(x)
+
+pprint(queue)
